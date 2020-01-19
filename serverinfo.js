98b73e3fe97c4c609b5bba29456a8f7f85e56f40
Knkajfw <oncewecanown@protonmail.com>
2020-01-19 14:00:02 +0800

    test

--- serverinfo.js
+++ serverinfo.js
@@ -1,3 +1,3 @@
 socketServerAddress = "https://paste-like-doinb.herokuapp.com";
-info = "点击<a id='top-info-bar-link1' href='#'>这里</a>获取新版本";
-infoLink = "https://www.baidu.com";
+info = "";
+infoLink = "";
