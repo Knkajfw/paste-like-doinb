d37ce0b6eb9834dce0dc349c8ac16616fd1cc76b
Knkajfw <oncewecanown@protonmail.com>
2020-03-26 11:24:50 +0800

    update serverinfo

--- serverinfo.js
+++ serverinfo.js
@@ -1,4 +1,5 @@
-socketServerAddress = "https://paste-like-doinb.herokuapp.com";
-info = "交流群699702899，服务器将迁回国内提升连接速度";
-infoLink = "";
-info2 = '';
+var socketServerAddress = "https://paste-like-doinb.herokuapp.com";
+var info = "交流群699702899，服务器将迁回国内提升连接速度";
+var infoLink = "";
+var info2 = '';
+var info2TargetVersion = 440331;
