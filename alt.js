4ad07143bb154579d67a8b7df15d8b487bbc40eb
Knkajfw <oncewecanown@protonmail.com>
2020-04-13 12:01:33 +0800

    alt.js pld-dev.h.com

--- alt.js
+++ alt.js
@@ -1,5 +1,5 @@
-var socketServerAddress = "https://www.indienost.com";
-var info = "交流群699702899，服务器将迁回国内提升连接速度";
+var socketServerAddress = "https://pld-dev.herokuapp.com";
+var info = "交流群699702899，1.3.0版本即将发布";
 var infoLink = "";
 var info2 = '';
 var info2TargetVersion = 440331;
