493af55a6cc7b834b00bf39f2091f00cf3e86924
Knkajfw <oncewecanown@protonmail.com>
2020-04-21 14:29:20 +0800

    update serverinfo

--- serverinfo.js
+++ serverinfo.js
@@ -1,5 +1,5 @@
 var socketServerAddress = "https://www.indienost.com";
-var info = "交流群699702899，1.3.0版本即将发布";
+var info = "交流群699702899，1.3.0版本已支持tp记录/一键同步";
 var infoLink = "";
 var info2 = '';
 var info2TargetVersion = 440331;
